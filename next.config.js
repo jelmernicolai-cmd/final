3c89babd83c74a303a23f5894731ccf1cee2cf8e
jelmernicolai-cmd <jelmer.nicolai@gmail.com>
2025-08-29 15:18:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports={reactStrictMode:true,i18n:{locales:['nl','en'],defaultLocale:'nl'}};
+const nextConfig = {
+  reactStrictMode: true
+};
+
+module.exports = nextConfig;
