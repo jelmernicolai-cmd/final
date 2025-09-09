8f20c4f0e1fc5d7fb7a75e2d5253dcfd443851a5
jelmernicolai-cmd <jelmer.nicolai@gmail.com>
2025-09-09 15:14:04 +0200

    Update Sidebar.tsx

--- components/portal/Sidebar.tsx
+++ components/portal/Sidebar.tsx
@@ -105,7 +105,7 @@ export default function Sidebar() {
   ];
 
   const SETTINGS_SUPPORT: Item[] = [
-    { href: "/app/settings", label: "Instellingen" },
+    { href: "/app/settings", label: "Instellingen!" },
     { href: "/contact", label: "Contact & Support" },
   ];
 
