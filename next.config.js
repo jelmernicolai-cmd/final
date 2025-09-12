e205481729493b89e9ef6576990826fd666274ee
jelmernicolai-cmd <jelmer.nicolai@gmail.com>
2025-09-12 22:40:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  // Zorg dat pdf-parse (CJS package) extern blijft en niet door Next gebundeld wordt
+  serverExternalPackages: ["pdf-parse"],
 };
 
 module.exports = nextConfig;
