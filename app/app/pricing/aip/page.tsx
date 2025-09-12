1ac6fe4303004a40ef69801c583fb1872051abd0
jelmernicolai-cmd <jelmer.nicolai@gmail.com>
2025-09-12 16:06:52 +0200

    Create page.tsx

---
+++ app/app/pricing/aip/page.tsx
@@ -0,0 +1,5 @@
+// app/app/pricing/aip/page.tsx
+export const metadata = { title: "AIP-master" };
+export default function Page() {
+  return <div className="mx-auto max-w-7xl px-4 py-6">AIP-beheer komt hier.</div>;
+}
